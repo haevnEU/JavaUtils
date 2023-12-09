c0070302ce51d95295c96977017d71be2bdc0a98
Nils M <nils.milewski@stud.hs-ruhrwest.de>
2023-12-09 16:28:05 +0100

    -Added   - AutoCollection annotation

--- src/main/java/de/haevn/utils/annotations/AnnotationUtils.java
+++ src/main/java/de/haevn/utils/annotations/AnnotationUtils.java
@@ -4,43 +4,40 @@ import java.io.File;
 import java.lang.annotation.Annotation;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.function.Consumer;
 
 public class AnnotationUtils {
 
+    private AnnotationUtils() {}
 
-
-    public static List<Class<?>> findClassesWithAnnotation(final String packageName, Class<? extends Annotation> annotation) {
+    public static List<Class<?>> collectBy(final String packageName, Class<? extends Annotation> annotation) {
         return getClasses(packageName).stream().filter(clazz -> clazz.isAnnotationPresent(annotation)).toList();
     }
 
-    public static List<Class<?>> getClasses(String packageName) {
-        List<Class<?>> classes = new ArrayList<>();
-        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-        String path = packageName.replace('.', '/');
-        URL resource = classLoader.getResource(path);
+    private static List<Class<?>> getClasses(final String packageName) {
+        final List<Class<?>> classes = new ArrayList<>();
+        final String path = packageName.replace('.', '/');
+        final URL resource = Thread.currentThread().getContextClassLoader().getResource(path);
+        if (null == resource) return List.of();
 
-        if (resource != null) {
-            File directory = new File(resource.getFile());
-            if (directory.exists()) {
-                File[] files = directory.listFiles();
-                if (files != null) {
-                    for (File file : files) {
-                        if (file.isDirectory()) {
-                            classes.addAll(getClasses(packageName + "." + file.getName()));
-                        } else if (file.getName().endsWith(".class")) {
-                            String className = packageName + '.' + file.getName().substring(0, file.getName().length() - 6);
-                            try {
-                                Class<?> clazz = Class.forName(className);
-                                classes.add(clazz);
-                            } catch (ClassNotFoundException e) {
-                                e.printStackTrace();
-                            }
-                        }
-                    }
+        final File directory = new File(resource.getFile());
+        if (!directory.exists()) return List.of();
+
+        final File[] files = directory.listFiles();
+        if (null == files) return List.of();
+        Arrays.stream(files).forEach(file -> {
+            if(file.isDirectory()) getClasses(packageName + "." + file.getName());
+            else if(file.getName().endsWith(".class")) {
+                String className = packageName + '.' + file.getName().substring(0, file.getName().length() - 6);
+                try {
+                    classes.add(Class.forName(className));
+                } catch (ClassNotFoundException e) {
+                    e.printStackTrace();
                 }
             }
-        }
+        });
         return classes;
     }
 
