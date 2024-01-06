e32eb004b7f89cc6d665dcb277d4d33b4e6c4e75
Nils M <nils.milewski@stud.hs-ruhrwest.de>
2024-01-06 19:27:08 +0100

    Added  - Method to access all running threads

--- src/main/java/de/haevn/utils/Core.java
+++ src/main/java/de/haevn/utils/Core.java
@@ -1,31 +1,37 @@
 package de.haevn.utils;
 
+
 import java.awt.*;
 import java.awt.datatransfer.Clipboard;
 import java.awt.datatransfer.StringSelection;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.function.Function;
 
 /**
  * A simple class for core information.
+ *
+ * @author haevn
  * @version 1.0
  * @since 1.0
- * @author haevn
  */
 public class Core {
     private static String appName = "";
     private static String appVersion = "";
-    private Core() {}
 
-    public static void setAppName(String appName) {
-        if(Core.appName.isBlank()) {
-            Core.appName = appName;
-        }
+    private Core() {
     }
 
     public static String getAppName() {
         return appName;
+    }
+
+    public static void setAppName(String appName) {
+        if (Core.appName.isBlank()) {
+            Core.appName = appName;
+        }
     }
 
     public static void copyText(String text) {
@@ -33,11 +39,11 @@ public class Core {
         clipboard.setContents(new StringSelection(text), null);
     }
 
-    public static <R, T> R map(T value, Function<? super T, ? extends R> mapper){
+    public static <R, T> R map(T value, Function<? super T, ? extends R> mapper) {
         return mapper.apply(value);
     }
 
-    public static <T> T mapRecord(T value, Function<? super T, ? extends T> mapper){
+    public static <T> T mapRecord(T value, Function<? super T, ? extends T> mapper) {
         return mapper.apply(value);
     }
 
@@ -52,7 +58,7 @@ public class Core {
         }
     }
 
-    public static void executeSecure(Runnable run){
+    public static void executeSecure(Runnable run) {
         try {
             run.run();
         } catch (Exception e) {
@@ -60,7 +66,7 @@ public class Core {
         }
     }
 
-    public static void setVersion(final String version){
+    public static void setVersion(final String version) {
         appVersion = version;
     }
 
@@ -69,27 +75,29 @@ public class Core {
     }
 
 
-
-    public static String getCurrentTime(){
+    public static String getCurrentTime() {
         return getCurrentDateWithFormat("HH:mm:ss");
     }
 
-    public static String getCurrentDate(){
+    public static String getCurrentDate() {
         return getCurrentDateWithFormat("dd/MM/yyyy");
     }
 
-    public static String getCurrentDateAndTime(){
+    public static String getCurrentDateAndTime() {
         return getCurrentDateWithFormat("dd/MM/yyyy HH:mm:ss");
     }
 
-    public static String getCurrentDateWithFormat(String format){
+    public static String getCurrentDateWithFormat(String format) {
         return new SimpleDateFormat(format).format(new Date());
     }
 
-    public static String[] getKeyValue(String key, String value){
+    public static String[] getKeyValue(String key, String value) {
         return new String[]{key, value};
     }
 
-
+    public static List<Thread> getRunningThreads() {
+        final var threads = Thread.getAllStackTraces().keySet();
+        return new ArrayList<>(threads);
+    }
 
 }
