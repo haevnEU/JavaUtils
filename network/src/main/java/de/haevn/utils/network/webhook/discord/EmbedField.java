66b3a2e3efede506e04e50c2e1c07534ad0a27a5
Nils M <nils.milewski@stud.hs-ruhrwest.de>
2024-06-25 00:50:04 +0200

    Refactored DiscordWebhook logic and removed lombok references

--- network/src/main/java/de/haevn/utils/network/webhook/discord/EmbedField.java
+++ network/src/main/java/de/haevn/utils/network/webhook/discord/EmbedField.java
@@ -4,10 +4,4 @@ public record EmbedField(String name, String value, boolean inline) {
     public EmbedField(String name, String value) {
         this(name, value, false);
     }
-
-    public EmbedField(String name, String value, boolean inline) {
-        this.name = name;
-        this.value = value;
-        this.inline = inline;
-    }
 }
--- network/src/main/java/de/haevn/utils/network/webhook/discord/EmbedFooter.java
+++ network/src/main/java/de/haevn/utils/network/webhook/discord/EmbedFooter.java
@@ -5,13 +5,7 @@ public record EmbedFooter(String text, String icon_url, String proxy_icon_url) {
         this(text, null, null);
     }
 
-    public EmbedFooter(String text, String icon_url) {
-        this(text, icon_url, null);
-    }
-
-    public EmbedFooter(String text, String icon_url, String proxy_icon_url) {
-        this.text = text;
-        this.icon_url = icon_url;
-        this.proxy_icon_url = proxy_icon_url;
+    public EmbedFooter(String text, String iconUrl) {
+        this(text, iconUrl, null);
     }
 }
